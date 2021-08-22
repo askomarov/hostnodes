565c6d8557c71b040a817334f80dc413fde14515
askomarov <askomarov13@mail.ru>
2021-08-22 20:49:07 +0300

    first commit

---
+++ build/js/initTyped.js
@@ -0,0 +1,20 @@
+var options = {
+  strings: [
+    "удаленно",
+    "сразу",
+    "пока учитесь",
+    "верстальщиком",
+    "фронтендером"
+  ],
+  // stringsElement: "#typed-strings",
+  typeSpeed: 40,
+  backSpeed: 30,
+  loop: true,
+  loopCount: Infinity
+};
+
+var typed = new Typed("#typed", options);
+
+
+
+// let typed = new Typed('#typed', options);
